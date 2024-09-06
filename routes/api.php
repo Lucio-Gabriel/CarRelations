0aa1a8d747800bb0598aca1774832a3fa3f69bf8
Gabriel <suppershock777@gmail.com>
2024-09-06 19:45:19 -0300

    Feat: created our routes and cars tables

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'findOne']);
+Route::post('/user', [UserController::class, 'insert']);
+
+Route::get('/cars', [CarController::class, 'index']);
+Route::get('/cars/{id}', [CarController::class, 'findOne']);
+Route::post('/cars', [CarController::class, 'insert']);
